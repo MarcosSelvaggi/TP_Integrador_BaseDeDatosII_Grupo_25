6347f3ecdd5798780fdea0b6edf490dbea850926
Emanuel Alejandro Celis <123424688+EANCelis@users.noreply.github.com>
2025-05-29 05:11:46 -0300

    Creadas las tablas de Estados de Pedidos, Métodos de Pago, Estado de Envío, Pedidos y Detalles de Pedidos/Ajuste DatosUsuarios

--- script Ecommerce_DB.sql
+++ script Ecommerce_DB.sql
@@ -19,7 +19,7 @@ insert into Rol values ('Usuario')
 create table Usuarios(
 	IdCliente int not null primary key identity(1,1),
 	CorreoElectronico NVARCHAR(100) not null, 
-	Contraseña VARCHAR(50) not null,
+	ContraseÃ±a VARCHAR(50) not null,
 	IdRol tinyint not null foreign key references Rol(IdRol),
 	Estado bit not null, 
 	FechaCreacion date not null
@@ -27,7 +27,7 @@ create table Usuarios(
 
 create table DatosUsuarios(
 	Documento int not null primary key, 
-	IdCliente int not null foreign key references Usuarios(IdCliente), 
+	IdCliente int not null unique foreign key references Usuarios(IdCliente), 
 	Nombre VARCHAR(100) not null, 
 	Apellido VARCHAR(100) not null,
 	NumTelefono VARCHAR(20) not null, 
@@ -51,4 +51,38 @@ create table Productos(
 	Stock int not null,
 	IdCategoria int not null foreign key references Categorias(IdCategoria),
 	IdMarca int not null foreign key references Marcas(IdMarca)
+)
+	
+create table EstadosPedidos (
+    IdEstadoPedido tinyint primary key,
+    Descripcion varchar(100)
+)
+
+create table MetodosPagos (
+    IdMetodoPago tinyint primary key,
+    Descripcion varchar(50)
+)
+
+create table EstadoEnvio (
+    IdEnvio tinyint primary key,
+    Descripcion varchar(100)
+)
+
+create table Pedidos (
+    IdPedido bigint primary key,
+    IdCliente int foreign key references Usuarios(IdCliente),
+    FechaPedido date not null default Getdate(),
+    IdEstadoPedido tinyint foreign key references EstadosPedidos(IdEstadoPedido),
+    IdMetodoPago tinyint foreign key references MetodosPagos(IdMetodoPago),
+    IdEnvio TINYINT foreign key references EstadoEnvio(IdEnvio)
+)
+
+create table DetallePedidos (
+    IdCliente int foreign key references Usuarios(IdCliente),
+    IdPedido bigint foreign key references Pedidos(IdPedido),
+    IdProducto int foreign key references Productos(IdProducto),
+    Cantidad tinyint,
+    PrecioUnitario money,
+    primary key (IdCliente, IdPedido)
+    --primary key (IdPedido, IdProducto)
 )
